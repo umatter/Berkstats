5bb3270ce6e9dcae16387b127c733273309d5ade
Ulrich Matter <ulrich.matter@unibas.ch>
2017-02-22 20:05:14 -0500

    Add render_all as function

--- code/render_all.R
+++ code/render_all.R
@@ -10,31 +10,31 @@ render_all <-
           
           if (is.null(file)) {
                # get paths of all RMD files in dir
-               paths <- list.files("notes", pattern = ".Rmd", full.names = TRUE)
+               files <- list.files("notes", pattern = ".Rmd", full.names = TRUE)
                
           } else {
-               paths <- paste0(dir, "/", file)
+               files <- paste0(dir, "/", file)
           }
           
           # render all
-          for (i in paths) {
+          for (f in files) {
                
                # self contained html page
-               render(input = i, output_format = html_document(self_contained = TRUE),
+               render(input = f, output_format = html_document(self_contained = TRUE),
                       output_dir = "notes/html", quiet = TRUE)
-               cat(paste0("Rendering ", i, " as HTML\n"))
+               cat(paste0("Rendering ", f, " as HTML\n"))
                # pdf
-               render(input = i, output_format = "pdf_document", 
+               render(input = f, output_format = "pdf_document", 
                       output_dir = "notes/pdf", quiet = TRUE)
-               cat(paste0("Rendering ", i, " as PDF\n"))
+               cat(paste0("Rendering ", f, " as PDF\n"))
                # markdown
-               render(input = i, output_format = "md_document",
+               render(input = f, output_format = "md_document",
                       output_dir = "notes/md",
                       quiet = TRUE)
-               cat(paste0("Rendering ", i, " as MD\n"))
+               cat(paste0("Rendering ", f, " as MD\n"))
                # R code
-               purl(input = i, documentation = 1)
-               cat(paste0("Generating ", i, " as R-Script\n"))
+               purl(input = f, documentation = 1)
+               cat(paste0("Generating ", f, " as R-Script\n"))
           }
      }
 
